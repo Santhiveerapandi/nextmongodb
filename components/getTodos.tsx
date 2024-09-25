ef3d64921b0d4046e8e55c4bb09b92babb819363
Santhiveerapandi.K <santhiveerapandi@gmail.com>
2024-09-25 19:07:15 +0530

    initial commit

---
+++ components/getTodos.tsx
@@ -0,0 +1,38 @@
+import { deleteTodo } from "@/lib/actions";
+import Todo from "@/models/todoModel";
+import React from "react";
+
+export default async function GetTodosUI() {
+
+  try {
+    const todos = await Todo.find();
+    if (todos.length === 0) {
+      return <h1>No todos</h1>;
+    } else {
+      return (
+        <div>
+          {todos.map((todo) => (
+            <div key={todo._id}>
+              <h3>{todo.todo as string}</h3>
+              <p>{todo.todoTime as number}</p>
+              <p>{todo._id.toString()}</p>
+              <form action={deleteTodo}>
+                <input
+                  hidden
+                  type="text"
+                  name="id"
+                  defaultValue={todo._id.toString()}
+                />
+                <button className="border rounded p-2 bg-red-400">
+                  delete
+                </button>
+              </form>
+            </div>
+          ))}
+        </div>
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ lib/actions.ts
@@ -0,0 +1,52 @@
+'use server';
+import Todo from "@/models/todoModel";
+import { revalidatePath } from "next/cache";
+import { connectToMongoDB } from "./db";
+
+export const createTodos = async (formData: FormData) => {
+    await connectToMongoDB();
+    // Extracting todo content and time from formData
+    const todo = formData.get("todo");
+    const todoDeadline = formData.get("todoDeadline");//req.body.todoDeadline);
+    try {
+
+        if ( !todoDeadline) {
+            console.log("Please fill out both fields.");
+            return;
+        }
+        // Convert date string to ISO date string format (if needed)
+    const todoData = {
+      todo,
+      todoDeadline: new Date(todoDeadline).toISOString(), // Ensure it's a valid date
+    };
+
+        // Creating a new todo using Todo model
+        const newTodo = await Todo.create(todoData);
+        // Saving the new todo
+        newTodo.save();
+        // Triggering revalidation of the specified path ("/")
+        revalidatePath("/");
+        // Returning the string representation of the new todo
+        return newTodo.toString();
+    } catch (error) {
+        console.log(error);
+        return {message: 'error creating todo'};
+    }
+};
+
+export const deleteTodo = async (id: FormData) => {
+    // Extracting todo ID from formData
+    const todoId = id.get("id");
+    try {
+        // Deleting the todo with the specified ID
+        await Todo.deleteOne({_id: todoId});
+        // Triggering revalidation of the specified path ("/")
+        revalidatePath("/");
+        // Returning a success message after deleting the todo
+        return ('todo deleted');
+    } catch (error) {
+        console.log(error);
+        // Returning an error message if todo deletion fails
+        return {message: 'error deleting todo'};
+    }
+}
